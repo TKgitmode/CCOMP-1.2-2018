60c6e466844b6fffa32c7d2e6c5ac569571f44c2
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:45:54 -0500

    cambios

--- code2/main.cpp
+++ code2/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    int a;
-    float b;
+    short int a;
+    long int b;
     cin >> a;
     cin >> b;
-    cout << " la resta de a y b:  "<< (a-b);
+    cout << "la resta de a y b:  "<< (a-b);
     cout << " la multiplicación de a y b:  " << (a*b);
     cout << " la división de a y b:  " << (a/b);
-    cout << "la suma de a y b:  " << (a+b)  << endl;
+    cout << " la suma de a y b:  " << (a+b)  << endl;
     return 0;
 }
